a2331940644bbd3aaa9613562a4cda17aa994126
Ronando Long <ronando.long@gmail.com>
2015-11-18 16:51:10 -0800

    It will display some animation in the first few images.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,4 +2,9 @@
 $(".question").click(function() {
   $(this).next().fadeToggle("fast");  // answer appears/disappears (next sibling)
   $(this).children().toggleClass("collapse");  // arrows appear/disappear (child)
+  $("#bear").animate({left: '300px', opacity: '0.475'}, "slow"); // animate: 2 styles
+  $("#camaro").animate({left: '300px'}, 1700); // animate: 1 style
+  $("#harly-48").animate({opacity: '0.70'}, 1200); // animate: 1 style
+  $("#cherries").animate({left: '400px', height: '230px', width: '225px' }, 3200, "linear"); // animate: 1 style
+
 });
